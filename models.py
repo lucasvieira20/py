8cc6262189696247472f84582601fade6dd79531
lucasvieira20 <web.lucasvieira@gmail.com>
2016-01-30 01:48:54 -0200

    Criando uma classe conta

--- models.py
+++ models.py
@@ -63,3 +63,21 @@ class Retangulo(object):
 
 	def obter_area(self):
 		return self.__area
+
+
+
+class Conta(object):
+	def __init__(self,titular,saldo):
+		self.titular = titular
+		self.saldo = saldo
+
+	def calcular_imposto(self):
+		self.saldo = self.saldo * 0.10
+		return self.saldo
+
+class ContaCorrente(Conta):
+	def __init__(self, titular,saldo):
+		super(ContaCorrente,self).__init__(titular,saldo)
+
+	def calcular_imposto(self):
+		return super(ContaCorrente, self).calcular_imposto() + 20
